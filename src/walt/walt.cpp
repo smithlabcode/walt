355d2606aaff5d2026637293bdcc67de57717f32
andrewdavidsmith <andrewds@usc.edu>
2018-08-09 19:45:13 -0700

    Generalized for accepting mixed single end and paired end files. Also lots of cleaning up.

--- src/walt/walt.cpp
+++ src/walt/walt.cpp
@@ -24,6 +24,9 @@
 
 #include <vector>
 #include <string>
+#include <stdexcept>
+
+#include <sys/stat.h>
 
 #include "smithlab_os.hpp"
 #include "OptionParser.hpp"
@@ -37,111 +40,112 @@ using std::vector;
 using std::string;
 using std::cerr;
 using std::endl;
+using std::runtime_error;
+using std::to_string;
+
+static void
+split_filenames(const string &s, vector<string> &filenames) {
+  std::stringstream ss(s);
+  using std::istream_iterator;
+  istream_iterator<string> begin(ss), end;
+  vector<string> vstrings(begin, end);
+  std::copy(vstrings.begin(), vstrings.end(), back_inserter(filenames));
+}
+
+static bool
+valid_suffix(const string &s, const vector<string> &v) {
+
+  bool found = false;
+  for (auto && i : v)
+    found = found || s.substr(s.length() - i.length()) == i;
+  return found;
+}
+
+
+static void
+validate_index_file(const string &index) {
+
+  struct stat st;
+  stat(index.c_str(), &st);
+  if (!S_ISREG(st.st_mode))
+    throw runtime_error("bad index file: " + index);
+
+  vector<string> suffixes = { "_CT00",
+                              "_CT01",
+                              "_GA10",
+                              "_GA11" };
+  for (auto && i : suffixes) {
+    const string suff_tab_filename = index + i;
+    stat(suff_tab_filename.c_str(), &st);
+    if (!S_ISREG(st.st_mode))
+      throw runtime_error("bad table file: " + suff_tab_filename);
+  }
+}
+
 
 int
 main(int argc, const char **argv) {
   try {
 
+    static const vector<string> fastq_suffixes = {".fastq", ".fq"};
+
     srand (time(NULL));
 
-    string command = argv[0];
-    bool help_info = false;
-    for (int i = 1; i < argc; i++) {
-      command += " ";
-      command += argv[i];
-      if (strcmp(argv[i], "-help") == 0 || strcmp(argv[i], "-about") == 0
-          || strcmp(argv[i], "-?") == 0) {
-        help_info = true;
-      }
-    }
+    string se_read_files_csv; // singled-end reads file or comma-sep list
 
-    if (argc > 1 && help_info == false) {
-      /* show the command line one the screen */
-      fprintf(stderr, "[WELCOME TO WALT v%s]\n", walt_version);
-      fprintf(stderr, "[%s", argv[0]);
-      for (int i = 1; i < argc; i++) {
-        fprintf(stderr, " %s", argv[i]);
-      }
-      fprintf(stderr, "]\n");
-    }
+    // paired-end reads files or comma-sep list
+    string pe_read_files_end1_csv, pe_read_files_end2_csv;
 
-    /* singled-end reads file, comma-separated list of files */
-    string reads_file_s;
-    vector<string> v_reads_file_s;
+    string index_file; // main index file
+    string output_files_csv; // output file or comma sep list
 
-    /* paired-end reads files, comma-separated list of files*/
-    string reads_file_p1;
-    string reads_file_p2;
-    vector<string> v_reads_file_p1;
-    vector<string> v_reads_file_p2;
+    /* WALT supports Tab-delimited SAM and MR output formats.  By
+     * default, WALT produces SAM format output files. To get MR
+     * format, the suffix of the output file should be ".mr". */
+    bool sam_format = false;
 
-    /* index file*/
-    string index_file;
+    string adaptor; // adaptor seq to trim at 3' end
 
-    /* output file */
-    string output_file;
-    vector<string> v_output_file;
+    bool ambiguous = false; // output ambiguous reads
+    bool unmapped = false; // output unmapped reads
 
-    /* WALT supports Tab-delimited SAM and MR output formats.
-     * By default, WALT produces SAM format output files. To
-     * get MR format, the suffix of the output file should be
-     * ".mr". */
-    bool SAM = true;
-
-    /* trimming adaptor sequence */
-    string adaptor;
-
-    /* paired-end or single-end mapping */
-    bool is_paired_end_reads = false;
-
-    /* output ambiguous or unmapped reads or not, both are false by default */
-    bool ambiguous = false;
-    bool unmapped = false;
-
-    /* AG_WILDCARD is false by default, which means that all Cs
-     * in the reads and genome are converted to Ts.
-     * If AG_WILDCARD is true, all Gs in the reads and genome
-     * are converted to As. If option is only for single-end mapping */
+    // you can use this if you don't understand what you are doing...
     bool AG_WILDCARD = false;
 
-    /* maximum allowed mismatches */
-    uint32_t max_mismatches = 6;
+    uint32_t max_mismatches = 6; // max mismatches allowed
 
-    /* number of reads to map at one loop */
-    uint32_t n_reads_to_process = 1000000;
-    const uint32_t PROCESS_LIMIT = 10000000;
+    /* number of reads to map at one batch */
+    uint32_t batch_size = 10000000;
+    const uint32_t max_batch_size = 100000000;
 
     /* ignore seeds which have more than b candidates */
     uint32_t b = 5000;
+    uint32_t top_k = 50; // (paired-end) candidates per end keep
+    int frag_range = 1000; // max dist btwn concordantly mapped mates
+    int num_of_threads = 1; // number of threads to use
 
-    /* paired-end reads: keep top k genome positions for each in the pair */
-    uint32_t top_k = 50;
-
-    /* max fragment length for paired end reads */
-    int frag_range = 1000;
-
-    /* number of threads for mapping */
-    int num_of_threads = 1;
+    bool VERBOSE = false;
 
     /****************** COMMAND LINE OPTIONS ********************/
     OptionParser opt_parse(strip_path(argv[0]), "map Illumina BS-seq reads","");
     opt_parse.add_opt("index", 'i', "index file created by makedb command "
                       "(the suffix of the index file should be '.dbindex')",
                       true, index_file);
-    opt_parse.add_opt("reads", 'r', "comma-separated list of read files "
+    opt_parse.add_opt("reads", 'r', "comma-sep list of read files "
                       "for singled-end mapping (expect suffix .fastq or .fq)",
-                      false, reads_file_s);
+                      false, se_read_files_csv);
     opt_parse.add_opt("reads1", '1', "comma-separated list of read files for "
                       "mate 1 (expect suffix .fastq or .fq)",
-                      false, reads_file_p1);
+                      false, pe_read_files_end1_csv);
     opt_parse.add_opt("reads2", '2', "comma-separated list of read files for "
                       "mate 2 (expect suffix .fastq or .fq)",
-                      false, reads_file_p2);
-    opt_parse.add_opt("output", 'o', "output file name", true, output_file);
+                      false, pe_read_files_end2_csv);
+    opt_parse.add_opt("output", 'o', "output file names (comma sep)",
+                      true, output_files_csv);
     opt_parse.add_opt("mismatch", 'm', "max allowed mismatches", false,
                       max_mismatches);
     opt_parse.add_opt("number", 'N', "number of reads per batch",
-                      false, n_reads_to_process);
+                      false, batch_size);
     opt_parse.add_opt("ambiguous", 'a', "output one random location for "
                       "ambiguously mapping reads in separate file",
                       false, ambiguous);
@@ -156,155 +160,99 @@ main(int argc, const char **argv) {
                       "(paired-end)", false, top_k);
     opt_parse.add_opt("fraglen", 'L', "max fragment length (paired-end)", false,
                       frag_range);
+    opt_parse.add_opt("sam", '\0', "output sam format", false, sam_format);
+    opt_parse.add_opt("verbose", 'v', "print more run info", false, VERBOSE);
     opt_parse.add_opt("thread", 't', "number of threads for mapping", false,
                       num_of_threads);
-
     vector<string> leftover_args;
     opt_parse.parse(argc, argv, leftover_args);
     if (argc == 1 || opt_parse.help_requested()) {
-      fprintf(stderr, "%s\n", opt_parse.help_message().c_str());
+      cerr << opt_parse.help_message() << endl;
       return EXIT_SUCCESS;
     }
     if (opt_parse.about_requested()) {
-      fprintf(stderr, "%s\n", opt_parse.about_message().c_str());
+      cerr << opt_parse.about_message() << endl;
       return EXIT_SUCCESS;
     }
     if (opt_parse.option_missing()) {
-      fprintf(stderr, "%s\n", opt_parse.option_missing_message().c_str());
+      cerr << opt_parse.option_missing_message() << endl;
       return EXIT_SUCCESS;
-    }
-    if (!is_valid_filename(index_file, "dbindex")) {
-      fprintf(stderr, "The suffix of the index file should be '.dbindex'\n");
-      return EXIT_FAILURE;
-    }
-    if (!reads_file_s.empty() && reads_file_p1.empty()
-        && reads_file_p2.empty()) {
-      is_paired_end_reads = false;
-    } else if (reads_file_s.empty() && !reads_file_p1.empty()
-        && !reads_file_p2.empty()) {
-      is_paired_end_reads = true;
-    } else {
-      fprintf(stderr, "Please use -r option to set singled-end reads, \n"
-              "-1 and -2 options to set paired-end reads\n");
-      return EXIT_FAILURE;
     }
     if (!leftover_args.empty()) {
-      fprintf(stderr, "%s\n", opt_parse.help_message().c_str());
+      cerr << opt_parse.help_message() << endl;
       return EXIT_SUCCESS;
     }
     /****************** END COMMAND LINE OPTIONS *****************/
 
-    bool get_empty_fields = false;
-    if (!is_paired_end_reads) {
-      v_reads_file_s = smithlab::split(reads_file_s, ",", get_empty_fields);
-      for (uint32_t i = 0; i < v_reads_file_s.size(); ++i) {
-        if (!is_valid_filename(v_reads_file_s[i], "fastq")
-            && !is_valid_filename(v_reads_file_s[i], "fq")) {
-          fprintf(stderr,
-                  "The suffix of the reads file should be '.fastq', '.fq'\n");
-          return EXIT_FAILURE;
-        }
-      }
-    } else {
-      v_reads_file_p1 = smithlab::split(reads_file_p1, ",", get_empty_fields);
-      v_reads_file_p2 = smithlab::split(reads_file_p2, ",", get_empty_fields);
-      if (v_reads_file_p1.size() != v_reads_file_p2.size()) {
-        fprintf(
-            stderr,
-            "For paired-end mapping, mate 1 and mate 2 should \n\
-                have the same number of files, and the paired files \n\
-                should be in the same order.");
-        return EXIT_FAILURE;
-      }
-      for (uint32_t i = 0; i < v_reads_file_p1.size(); ++i) {
-        if (!is_valid_filename(v_reads_file_p1[i], "fastq")
-            && !is_valid_filename(v_reads_file_p1[i], "fq")) {
-          fprintf(stderr,
-                  "The suffix of the reads file should be '.fastq', '.fq'\n");
-          return EXIT_FAILURE;
-        }
-      }
-    }
+    validate_index_file(index_file);
 
-    if (!is_paired_end_reads) {
-      if (v_reads_file_s.size() == 1) {
-        v_output_file.push_back(output_file);
-      } else {
-        char output_filename[1000];
-        for (uint32_t i = 0; i < v_reads_file_s.size(); ++i) {
-          sprintf(output_filename, "%s_s%u", output_file.c_str(), i);
-          v_output_file.push_back(output_filename);
-        }
-      }
-    } else {
-      if (v_reads_file_p1.size() == 1) {
-        v_output_file.push_back(output_file);
-      } else {
-        char output_filename[1000];
-        for (uint32_t i = 0; i < v_reads_file_p1.size(); ++i) {
-          sprintf(output_filename, "%s_p%u", output_file.c_str(), i);
-          v_output_file.push_back(output_filename);
-        }
-      }
-    }
+    vector<string> se_read_files;
+    split_filenames(se_read_files_csv, se_read_files);
+    for (auto && i : se_read_files)
+      if (!valid_suffix(i, fastq_suffixes))
+        throw runtime_error("read file invalid suffix: " + i);
 
-    for (uint32_t i = 0; i < v_reads_file_p2.size(); ++i) {
-      if (!is_valid_filename(v_reads_file_p2[i], "fastq")
-          && !is_valid_filename(v_reads_file_p2[i], "fq")) {
-        fprintf(stderr,
-                "The suffix of the reads file should be '.fastq', '.fq'\n");
-        return EXIT_FAILURE;
-      }
-    }
+    vector<string> pe_read_files_end1;
+    split_filenames(pe_read_files_end1_csv, pe_read_files_end1);
 
-    size_t suffix_pos = output_file.find_last_of(".");
-    if (suffix_pos == string::npos) {
-      SAM = true;
-    } else if (".mr" == output_file.substr(suffix_pos)) {
-      SAM = false;
-    }
-    /****************** END COMMAND LINE OPTIONS *****************/
+    vector<string> pe_read_files_end2;
+    split_filenames(pe_read_files_end2_csv, pe_read_files_end2);
+
+    if (pe_read_files_end1.size() != pe_read_files_end2.size())
+      throw runtime_error("unequal number of end1 and end2 files");
+
+    for (auto && i : pe_read_files_end1)
+      if (!valid_suffix(i, fastq_suffixes))
+        throw runtime_error("read file invalid suffix: " + i);
+
+    for (auto && i : pe_read_files_end2)
+      if (!valid_suffix(i, fastq_suffixes))
+        throw runtime_error("read file invalid suffix: " + i);
+
+    vector<string> output_files;
+    split_filenames(output_files_csv, output_files);
+    if (output_files.size() !=
+        se_read_files.size() + pe_read_files_end1.size())
+      throw runtime_error("wrong number of output files: " +
+                          output_files_csv);
 
     //////////////////////////////////////////////////////////////
     // CHECK OPTIONS
-    fprintf(stderr, "[MAXIMUM NUMBER OF MISMATCHES IS %u]\n", max_mismatches);
-    fprintf(stderr, "[NUMBER OF THREADS FOR MAPPING IS %d]\n", num_of_threads);
+    if (VERBOSE)
+      cerr << "max_mismatches: " << max_mismatches << endl
+           << "threads: " << num_of_threads << endl;
 
-    if (n_reads_to_process > PROCESS_LIMIT) {
-      n_reads_to_process = PROCESS_LIMIT;
-    }
+    if (batch_size > max_batch_size)
+      throw runtime_error("batch size may not exceed" +
+                          to_string(max_batch_size));
 
-    if (is_paired_end_reads && top_k < 2) {
-      fprintf(stderr, "-k option should be at least 2 for paired-end reads\n");
-      return EXIT_FAILURE;
-    }
+    if (top_k < 2 || top_k > 300)
+      throw runtime_error("paired-end candidates must be in [2, 300]");
 
-    if (is_paired_end_reads && top_k > 300) {
-      fprintf(stderr,
-              "-k option should be less than 300 for paired-end reads\n");
-      return EXIT_FAILURE;
-    }
-    ShowGenomeInfo(index_file);
+    if (VERBOSE)
+      ShowGenomeInfo(index_file);
 
-    //////////////////////////////////////////////////////////////
-    // Mapping
-    if (!is_paired_end_reads) {
-      for (uint32_t i = 0; i < v_reads_file_s.size(); ++i) {
-        ProcessSingledEndReads(command, index_file, v_reads_file_s[i],
-                               v_output_file[i], n_reads_to_process,
-                               max_mismatches, b, adaptor, AG_WILDCARD,
-                               ambiguous, unmapped, SAM, num_of_threads);
-      }
-    }
-    else {
-      for (uint32_t i = 0; i < v_reads_file_p1.size(); ++i) {
-        ProcessPairedEndReads(command, index_file, v_reads_file_p1[i],
-                              v_reads_file_p2[i], v_output_file[i],
-                              n_reads_to_process, max_mismatches, b, adaptor,
-                              top_k, frag_range, ambiguous, unmapped,
-                              SAM, num_of_threads);
-      }
-    }
+    // map any single-end reads
+    const size_t n_se_read_files = se_read_files.size();
+    if (VERBOSE)
+      cerr << "n_se_read_files: " << n_se_read_files << endl;
+    for (uint32_t i = 0; i < se_read_files.size(); ++i)
+      ProcessSingledEndReads(VERBOSE, index_file, se_read_files[i],
+                             output_files[i], batch_size,
+                             max_mismatches, b, adaptor, AG_WILDCARD,
+                             ambiguous, unmapped, sam_format, num_of_threads);
+
+    // now map any paired-end reads
+    const size_t n_pe_read_files = pe_read_files_end1.size();
+    if (VERBOSE)
+      cerr << "n_pe_read_files: " << n_pe_read_files << endl;
+    for (uint32_t i = 0; i < n_pe_read_files; ++i)
+      ProcessPairedEndReads(VERBOSE, index_file,
+                            pe_read_files_end1[i], pe_read_files_end2[i],
+                            output_files[i],
+                            batch_size, max_mismatches, b, adaptor,
+                            top_k, frag_range, ambiguous, unmapped,
+                            sam_format, num_of_threads);
   }
   catch (const std::runtime_error &e) {
     cerr << e.what() << endl;
